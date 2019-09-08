b5700e5b9c9ed6ffa009bcff1b768024aa056502
youvovk <wer3wollf@gmail.com>
2019-09-08 12:00:53 +0300

    Updates

---
+++ precache-manifest.096f755ed837416512222b1d370c8975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d155c2bce4bcd056e7f48dd0571df3",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "68411d9f1ea8d43fff87",
+    "url": "/react_static-list-of-todos/static/css/main.2f5c7fce.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_static-list-of-todos/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "68411d9f1ea8d43fff87",
+    "url": "/react_static-list-of-todos/static/js/main.8ede2be0.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  },
+  {
+    "revision": "1fdd2ebf0be1f6924f7267abfbd2a0cf",
+    "url": "/react_static-list-of-todos/static/media/ch.1fdd2ebf.svg"
+  },
+  {
+    "revision": "231a7dcbabe4604443041df252673f74",
+    "url": "/react_static-list-of-todos/static/media/folder.231a7dcb.svg"
+  },
+  {
+    "revision": "63cb4921cba1eb35818db9277cb3a18d",
+    "url": "/react_static-list-of-todos/static/media/id-card.63cb4921.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.096f755ed837416512222b1d370c8975.js"
 );
 
 self.addEventListener('message', (event) => {
